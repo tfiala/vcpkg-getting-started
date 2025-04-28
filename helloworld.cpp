a11f9febf9ee1b939b1ff75864fb1337ad8fbc80
Todd Fiala <todd.fiala@gmail.com>
2025-04-27 21:31:40 -0700

    Added valid usage of ibkr-flex-statement.

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,24 @@
+#include <filesystem>
 #include <fmt/core.h>
+#include <ibkr_flex_statement.h>
 
-int main()
+int main(int argc, const char **argv)
 {
-    fmt::print("Hello World!\n");
+    if (argc < 2)
+    {
+        fmt::print("Hello World!\n");
+    }
+    else
+    {
+        auto filename = std::string{argv[1]};
+        if (std::filesystem::exists(filename))
+        {
+            ibkr_flex_statement::parse_statement_file(filename.c_str());
+        }
+        else
+        {
+            fmt::print("filename {} does not exist\n", filename);
+        }
+    }
     return 0;
 }
